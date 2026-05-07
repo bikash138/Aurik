21373b7e4a9f234c2e8c5c50408655b1e30fb875
Bikash Shaw <vshaw138@gmail.com>
2026-05-07 16:29:04 +0530

    add database package

---
+++ packages/database/src/db.ts
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "./generated/prisma/client.js";
+
+/*This connection string is typically used during development or when running scripts
+ directly from this package. In production, the DATABASE_URL will be pulled from 
+ the environment of the application that imports this package.
+*/
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
+export { ClientType, VerificationTokenType } from "./generated/prisma/enums.js";
